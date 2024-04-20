1b0d1bf0ff2b980dbb92e8b171b52f49599985ae
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2024-04-20 18:13:01 +0900

    Refactor

--- scripts/update.sh
+++ scripts/update.sh
@@ -13,7 +13,7 @@ while read -r p; do
   current_version=$(echo "$p" | cut -d ' ' -f 2)
   latest_version=$(asdf latest "$tool")
   if [ "$current_version" = "$latest_version" ]; then
-    logInfo "$tool $latest_version is already installed"
+    log_info "$tool $latest_version is already installed"
   else
     sed -i.bak -E "s/($tool )[0-9.pre-]+(.*)/\1$latest_version\2/g" "$ROOT_PATH/.tool-versions.bak"
     asdf install "$tool" "$latest_version"
