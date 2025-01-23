5b12bf151fa3c58f1637ced389a70cca293f414c
fabasoad <fabasoad@gmail.com>
2025-01-23 14:35:21 +0900

    Fix Dockerfile

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -10,13 +10,28 @@ hadolint_version="$(sh "${SCRIPTS_BASE_FOLDER_PATH}/get-tool-version.sh" -t hado
 pre_commit_version="$(sh "${SCRIPTS_BASE_FOLDER_PATH}/get-tool-version.sh" -t pre-commit)"
 terraform_version="$(sh "${SCRIPTS_BASE_FOLDER_PATH}/get-tool-version.sh" -t terraform)"
 
-podman build \
+if command -v podman >/dev/null 2>&1; then
+  cmd="podman"
+else
+  cmd="docker"
+fi
+
+case "$(uname -m)" in
+  arm64|aarch64)
+    platform="linux/arm64/v8"
+    ;;
+  *)
+    platform="linux/amd64"
+    ;;
+esac
+
+${cmd} build \
   --tag "pre-commit-container:$(date +%s)" \
   --tag "pre-commit-container:latest" \
-  --build-arg "ARCH=$(uname -m)" \
   --build-arg "ACTIONLINT_VERSION=${actionlint_version}" \
   --build-arg "HADOLINT_VERSION=${hadolint_version}" \
   --build-arg "PRE_COMMIT_VERSION=${pre_commit_version}" \
   --build-arg "TERRAFORM_VERSION=${terraform_version}" \
+  --platform "${platform}" \
   --file "${SRC_PATH}/Dockerfile" \
   .
