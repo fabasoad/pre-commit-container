f43b616ed616a7b000ca40bb156583c5cd98d80f
Eugene <fabasoad@gmail.com>
2025-02-06 23:49:26 +0900

    Remove versions from Dockerfile (#25)

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 
 SCRIPT_PATH=$(realpath "$0")
-SCRIPTS_BASE_FOLDER_PATH=$(dirname "$SCRIPT_PATH")
-ROOT_PATH=$(dirname "$SCRIPTS_BASE_FOLDER_PATH")
+SCRIPTS_DIR_PATH=$(dirname "${SCRIPT_PATH}")
+ROOT_PATH=$(dirname "${SCRIPTS_DIR_PATH}")
+LIB_DIR_PATH="${SCRIPTS_DIR_PATH}/lib"
 
-# shellcheck source=scripts/common.sh
-. "$SCRIPTS_BASE_FOLDER_PATH/common.sh"
+. "${LIB_DIR_PATH}/logging.sh"
 
-cp "$ROOT_PATH/.tool-versions" "$ROOT_PATH/.tool-versions.bak"
+cp "${ROOT_PATH}/.tool-versions" "${ROOT_PATH}/.tool-versions.bak"
 while read -r p; do
-  tool=$(echo "$p" | cut -d ' ' -f 1)
-  current_version=$(echo "$p" | cut -d ' ' -f 2)
-  latest_version=$(asdf latest "$tool")
-  if [ "$current_version" = "$latest_version" ]; then
-    log_info "$tool $latest_version is already installed"
+  tool=$(echo "${p}" | cut -d ' ' -f 1)
+  current_version=$(echo "${p}" | cut -d ' ' -f 2)
+  latest_version=$(asdf latest "${tool}")
+  if [ "${current_version}" = "${latest_version}" ]; then
+    log_info "${tool} ${latest_version} is already installed"
   else
-    sed -i.bak -E "s/($tool )[0-9.pre-]+(.*)/\1$latest_version\2/g" "$ROOT_PATH/.tool-versions.bak"
-    asdf install "$tool" "$latest_version"
+    sed -i.bak -E "s/(${tool} )[0-9.pre-]+(.*)/\1${latest_version}\2/g" "${ROOT_PATH}/.tool-versions.bak"
+    asdf install "${tool}" "${latest_version}"
   fi
-done <"$ROOT_PATH/.tool-versions"
-mv -f "$ROOT_PATH/.tool-versions.bak" "$ROOT_PATH/.tool-versions"
-rm -f "$ROOT_PATH/.tool-versions.bak*"
+done <"${ROOT_PATH}/.tool-versions"
+mv -f "${ROOT_PATH}/.tool-versions.bak" "${ROOT_PATH}/.tool-versions"
+rm -f "${ROOT_PATH}/.tool-versions.bak*"
